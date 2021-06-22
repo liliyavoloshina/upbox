9e43b9258a382aec969f1139f8df7c27c59da658
Liliya <liliyavoloshine@gmail.com>
2021-06-22 09:25:55 +0400

    edited vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: './'
+	base: './',
+	css: {
+		rootpath: "http://localhost:3000/assets"
+    // preprocessorOptions: {
+    //   less: {
+    //     rootpath: "http://localhost:3000/src",
+    //   },
+    // },
+  },
 })
