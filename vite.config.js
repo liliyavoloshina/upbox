2431d2d18e4fbe586eafe5b3282feeb8456cc75c
Liliya <liliyavoloshine@gmail.com>
2021-06-22 09:37:30 +0400

    edited vite config and changed image path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: './',
-	css: {
-		rootpath: 'http://localhost:3000/assets'
-	}
+	base: './'
 })
